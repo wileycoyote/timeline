5d25090388a2a0a385db589fd16e6c680ef3d1fd
Roger Day <Roger Day>
2026-02-14 15:52:46 +0000

    Safety checkin

--- src/timeline/show.py
+++ src/timeline/show.py
@@ -4,10 +4,12 @@ import pprint as pp
 from db.database import engine
 from sqlalchemy import select
 from db.models import TimelineGroup, Timeline
+from datetime import datetime as dt
+import pandas_log
+import numpy as np
 
-
-def_start_frame = 1500
-def_end_frame = 1526
+def_start_frame = dt.fromisoformat("1500-01-01")
+def_end_frame = dt.fromisoformat("1526-01-01")
 range_units = 54
 
 conn = None
@@ -17,7 +19,6 @@ def get_data():
 
     stmt = select(TimelineGroup).order_by("order")
     # set up the dataframe for timeline groups
-    import pdb; pdb.set_trace()
     t_df = pd.read_sql_query(
         stmt,
         con=engine,
@@ -26,27 +27,31 @@ def get_data():
     # set up the dataframe for timelines
     stmt = select(Timeline, TimelineGroup.label.label("timeline")).where(
         TimelineGroup.id == Timeline.parent)
-    e_df = pd.read_sql_query(
-        stmt,
-        con=engine,
-        index_col='start'
-    )
+    with pandas_log.enable():
+        e_df = pd.read_sql_query(
+            stmt,
+            con=engine,
+        )
     return t_df, e_df
 
 
 def get_timelines_slice(df, s, e):
     # get all the events columns that intersect our time-slice
-    q = f"start > '{s}' and start <= '{e}'"
-    slice_df = df.query(q)
+    #
+    # This would be better using df.query() but I can't work out why
+    # I couldn't get it to work
+    pp.pprint(s)
+    pp.pprint(e)
+    slice_df = df.loc[(df['start'] >= s) & (df['start'] < e)]
     return slice_df
 
 
 def run_app():
     # this initialises the data
-    # import pdb; pdb.set_trace()
     t_df, e_df = get_data()
     # this navigates the dates
     events_slice = get_timelines_slice(e_df, def_start_frame, def_end_frame)
+    pp.pprint(events_slice)
     timeline_names = events_slice['timeline'].unique()
 
     # to allow for distribution of horizontal timelines, this
@@ -56,7 +61,9 @@ def run_app():
         #
         # set the top axis in years
         # set default values for now
-        years = [str(x) for x in range(def_start_frame, def_end_frame, 1)]
+        year_start = def_start_frame.year
+        year_end = def_end_frame.year
+        years = [str(x) for x in range(year_start, year_end, 1)]
         posn = [x for x in range(2, range_units, 2)]
         # Remove the y-axis and some spines.
         ax.yaxis.set_visible(False)
@@ -65,25 +72,22 @@ def run_app():
         ax.xaxis.tick_top()
         ax.set_xticks(posn, years)
         ax.margins(y=0.2)
-
         # do the timelines
         for t in timeline_names:
             # get the timeline display data
-            import pdb; pdb.set_trace()
             timeline_data = t_df.query(f"label  == '{t}'")
-            pp.pprint(timeline_data)
             # get the associated events data
             events = events_slice.query(f"timeline == '{t}'")
-            pp.pprint(events)
-            pp.pprint(timeline_data)
             if timeline_data['line_type'].item() == 'continuous':
-                points = events.index.values
-                plt.plot(1, points, marker='o', linestyle='-')
+                points = events['start'].values
+#                 import pdb; pdb.set_trace()
+                x = np.arange(0, len(points), 1)
+                plt.plot(x, points, marker='o', linestyle='-')
                 for label in events['label']:
                     # need to set up dates as DateTime
                     plt.annotate(
                         f'{label}',
-                        (1, points),
+                        (x, points),
                         textcoords="offset points",
                         xytext=(0, 10),
                         ha='center')
