80ef9d25865361021eb72916750dc612eb241594
Roger Day <Roger Day>
2026-03-13 16:15:02 +0000

    Daily safety checkin
    TODO: make sure Dates, not Datetimes are passed through to dates

--- src/timeline/plot.py
+++ src/timeline/plot.py
@@ -1,20 +1,4 @@
-
-
-
-
-
-
-
-
-
-
-
 import matplotlib.pyplot as plt
-
-
-
-
-kkkk
 
 import pandas as pd
 import pprint as pp
@@ -24,6 +8,7 @@ from db.models import TimelineGroup, Timeline
 from datetime import datetime as dt
 import pandas_log
 import numpy as np
+import matplotlib.dates as mdates
 
 def_start_frame = dt.fromisoformat("1500-01-01")
 def_end_frame = dt.fromisoformat("1526-01-01")
@@ -34,7 +19,7 @@ conn = None
 
 def get_data():
 
-    stmt = select(TimelineGroup).order_by("order")
+    stmt = select(TimelineGroup)
     # set up the dataframe for timeline groups
     t_df = pd.read_sql_query(
         stmt,
@@ -42,7 +27,7 @@ def get_data():
         index_col='id'
     )
     # set up the dataframe for timelines
-    stmt = select(Timeline, TimelineGroup.label.label("timeline")).where(
+    stmt = select(Timeline, TimelineGroup).where(
         TimelineGroup.id == Timeline.parent)
     with pandas_log.enable():
         e_df = pd.read_sql_query(
@@ -62,22 +47,21 @@ def get_timelines_slice(df, s, e):
 def run_app():
     # this initialises the data
     t_df, e_df = get_data()
+    # import pdb;pdb.set_trace()
     # this navigates the dates
     events_slice = get_timelines_slice(e_df, def_start_frame, def_end_frame)
-    pp.pprint(events_slice)
-    pp.pprint(t_df)
-    timeline_names = events_slice['timeline'].unique()
-
+    dates = events_slice['start']
     # to allow for distribution of horizontal timelines, this
     with plt.style.context('Solarize_Light2'):
         fig, ax = plt.subplots(figsize=(20, 14), layout="constrained")
         ax.set(title="Timelines for 1300 to 1600")
         #
         # The baseline.
-        ax.axhline(0, c="black")
+        # ax.axhline(0, c="black")
         #
         # The markers on the baseline.
-        ax.plot(dates, np.zeros_like(dates), "ko", mfc="white")
+        import pdb; pdb.set_trace()
+        ax.plot(dates.Date, np.zeros_like(dates), "ko", mfc="white")
         #
         # set the top axis in years
         # set default values for now
@@ -87,27 +71,33 @@ def run_app():
         posn = [x for x in range(2, range_units, 2)]
         # Remove the y-axis and some spines.
         ax.yaxis.set_visible(False)
-        ax.spines[["bottom", "right"]].set_visible(False)
-        ax.invert_yaxis()
-        ax.xaxis.tick_top()
+        ax.spines[["left", "top", "bottom", "right"]].set_visible(False)
+        ax.spines[["bottom"]].set_position(("axes", 0.5))
+        # ax.xaxis.tick_top()
         ax.set_xticks(posn, years)
         ax.margins(y=0.2)
-        # do the timelines
-        for t in timeline_names:
-            # get the timeline display data
-            timeline_data = t_df.query("label  == @t")
-            # get the associated events data
-            events = events_slice.query("timeline == @t")
-            level = timeline_data['level'].item()
-                points = events['start'].values
-                x = np.arange(0, len(points), 1)
-                plt.plot(x, points, marker='o', linestyle='-')
-                for label in events['label']:
-                    # need to set up dates as DateTime
-                    plt.annotate(
-                        f'{label}',
-                        (x, points),
-                        textcoords="offset points",
-                        xytext=(0, 10),
-                        ha='center')
+        ax.set_ylim(-7, 7)
+        # import pdb; pdb.set_trace()
+        # annotate the points on the horizontal line
+        for e, event in events_slice.iterrows():
+            pp.pprint(event)
+            level = event['level']
+            dt = event['start']
+            label = event['label']
+            colour = event['colour']
+            plt.annotate(
+                label,
+                xy=(dt,
+                    0.1 if level > 0 else -0.1),
+                xytext=(dt, level),
+                textcoords="offset points",
+                arrowprops=dict(
+                    arrowstyle="-",
+                    color=colour,
+                    linewidth=0.8),
+                ha='center',
+            )
+        ax.xaxis.set(
+            major_locator=mdates.YearLocator(),
+            major_formatter=mdates.DateFormatter("%Y"))
     plt.show()
